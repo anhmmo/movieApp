f30d01fd32f72c98a125d90fa265a94af6183b3d
anhmmo <aun@metropolia.fi>
2020-06-01 11:42:56 +0300

    update new project to root

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import BabyManager from './Components/App/App';
+import App from './components/App';
 
-
-ReactDOM.render(<BabyManager />, document.getElementById('container'));
-
+ReactDOM.render(<App />, document.getElementById('root'));
