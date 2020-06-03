add9261264311ca1da10fce51f7e51d69e9f10f2
anhmmo <aun@metropolia.fi>
2020-06-03 09:29:03 +0300

    update SlideMovie style

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,69 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   POPULAR_BASE_URL,
   SEARCH_BASE_URL,
   POSTER_SIZE,
-  BACKDROP_SIZE,
   IMAGE_BASE_URL,
-} from '../config';
+} from "../config";
 
 // import Components
-import HeroImage from './elements/HeroImage';
-import SearchBar from './elements/SearchBar';
-import Grid from './elements/Grid';
-import MovieThumb from './elements/MovieThumb';
-import LoadMoreBtn from './elements/LoadMoreBtn';
-import Spinner from './elements/Spinner';
+
+import Grid from "./elements/Grid";
+import MovieThumb from "./elements/MovieThumb";
+import LoadMoreBtn from "./elements/LoadMoreBtn";
+import Spinner from "./elements/Spinner";
 
 // Custom Hook
-import { useHomeFetch } from './hooks/useHomeFetch';
+import { useHomeFetch } from "./hooks/useHomeFetch";
 
-import NoImage from './images/no_image.jpg';
+import NoImage from "./images/no_image.jpg";
+import SlideMovie from "./elements/SlideMovie";
 
 const Home = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [
     {
-      state: { movies, currentPage, totalPages, heroImage },
+      state: { movies, currentPage, totalPages },
       loading,
       error,
     },
     fetchMovies,
   ] = useHomeFetch(searchTerm);
 
-  const searchMovies = search => {
+  const searchMovies = (search) => {
     const endpoint = search ? SEARCH_BASE_URL + search : POPULAR_BASE_URL;
 
     setSearchTerm(search);
     fetchMovies(endpoint);
-
-  }
+  };
 
   const loadMoreMovies = () => {
-    const searchEndpoint = `${SEARCH_BASE_URL}${searchTerm}&page=${currentPage + 1}`;
+    const searchEndpoint = `${SEARCH_BASE_URL}${searchTerm}&page=${
+      currentPage + 1
+    }`;
     const popularEndpoint = `${POPULAR_BASE_URL}&page=${currentPage + 1}`;
 
     const endpoint = searchTerm ? searchEndpoint : popularEndpoint;
 
     fetchMovies(endpoint);
-
-  }
+  };
 
   if (error) return <div>Something went wrong ...</div>;
   if (!movies[0]) return <Spinner />;
 
   return (
     <>
-      {!searchTerm && (
-        <HeroImage
-          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
-          title={heroImage.original_title}
-          text={heroImage.overview}
-        />
-      )}
-      <SearchBar callback={searchMovies} />
-      <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
-        {movies.map(movie => (
+      {!searchTerm && <div></div>}
+      <SlideMovie />
+      <Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
+        {movies.map((movie) => (
           <MovieThumb
             key={movie.id}
             clickable
