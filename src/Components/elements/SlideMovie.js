0073e789cb1cc0d06e6e1d1954237e2582951abd
anhmmo <aun@metropolia.fi>
2020-06-07 16:51:30 +0300

    create to function: openNextMoive and openPrevMovie

--- src/Components/elements/SlideMovie.js
+++ src/Components/elements/SlideMovie.js
@@ -1,251 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SlideMovie.scss";
 
-function SlideMovie(props) {
+function SlideMovie({ movie }) {
+  const [moviesList, setMoviesList] = useState(movie);
+
+  const openNextMovie = () => {
+    let movie2 = moviesList.slice(0, 5);
+    setMoviesList([...moviesList.slice(5), ...movie2]);
+  };
+  const openPrevMovie = () => {
+    let movie2 = moviesList.splice(15, 5);
+    setMoviesList([...movie2, ...moviesList]);
+  };
+
   return (
     <section className="slider-show">
       <div className="slider-container">
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Ad Astra (2017)</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/aQvJ5WPzZgYVDrxLX4R6cLJCEaQ.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Sonic the Hedgehogh</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/niyXFhGIk4W2WTcX2Eod8vx2Mfe.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Survive the Night</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/jHo2M1OiH9Re33jYtUQdfzPeUkx.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Scoob!</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/avl9MEQhtvokNnzoWepkmHBZ2ss.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/4q2NNj4S5dG2RLF9CpXsej7yXl.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/qa6HCwP4Z15l3hpsASz3auugEW6.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/y95lQLnuNKdPAzw9F9Ab8kJ80c3.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/avl9MEQhtvokNnzoWepkmHBZ2ss.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/4q2NNj4S5dG2RLF9CpXsej7yXl.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/qa6HCwP4Z15l3hpsASz3auugEW6.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/y95lQLnuNKdPAzw9F9Ab8kJ80c3.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/avl9MEQhtvokNnzoWepkmHBZ2ss.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/4q2NNj4S5dG2RLF9CpXsej7yXl.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/qa6HCwP4Z15l3hpsASz3auugEW6.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
-        <div className="slider-box">
-          <img
-            className="slider-image"
-            src="http://image.tmdb.org/t/p/w500/y95lQLnuNKdPAzw9F9Ab8kJ80c3.jpg"
-            alt="film"
-          />
-          <i class="far fa-play-circle"></i>
-          <p className="slider-rating">8/10</p>
-          <h2>Bloodshot</h2>
-          <p className="slider-genres">Action, drama</p>
-          <p className="slider-cast">cast: nguyen au</p>
-        </div>
+        {moviesList.map((film, index) => (
+          <div key={index} className="slider-box">
+            <img
+              className="slider-image"
+              src={"http://image.tmdb.org/t/p/w500" + film.poster_path}
+              alt="film"
+            />
+            <i className="far fa-play-circle"></i>
+            <p className="slider-rating">{film.vote_average}/10</p>
+            <h2>{film.title}</h2>
+            <p className="slider-genres">Action, drama </p>
+            <p className="slider-cast">{film.release_date}</p>
+          </div>
+        ))}
       </div>
+      <button onClick={() => openPrevMovie()}>prev</button>
+      <button onClick={() => openNextMovie()}>next</button>
     </section>
   );
 }
