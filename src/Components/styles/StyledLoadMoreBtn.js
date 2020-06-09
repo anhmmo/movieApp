b936402b75f25c23fafe084fd579f9ac6c7b64ea
anhmmo <aun@metropolia.fi>
2020-06-09 19:09:07 +0300

    change button style

--- src/Components/styles/StyledLoadMoreBtn.js
+++ src/Components/styles/StyledLoadMoreBtn.js
@@ -1,23 +1,35 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledLoadMoreBtn = styled.button`
-  background: #000;
-  width: 25%;
-  min-width: 200px;
-  height: 70px;
-  color: #fff;
-  cursor: pointer;
-  transition: all 0.3s;
-  border-radius: 40px;
-  font-family: 'Abel', sans-serif;
-  font-size: 28px;
-  max-width: 1280px;
   display: block;
   margin: 20px auto;
-  padding: 0 20px;
-  outline: none;
+  padding: 3px 36px 2px 36px;
 
+  outline: none;
+  border: 1px solid rgba(0, 0, 0, 0);
+  border-radius: 22px;
+  box-shadow: 0px 3px 0px #d40e1a;
+  -webkit-box-shadow: 0px 3px 0px #d40e1a;
+
+  background: #ff415b;
+  color: #fff;
+
+  font-family: "Balsamiq Sans", cursive;
+  font-size: 23px;
+  text-transform: capitalize;
+  line-height: 40px;
+
+  cursor: pointer;
+  transition: all 200ms;
+
+  :active,
+  :link,
+  :visited {
+    box-shadow: none;
+    -webkit-box-shadow: none;
+    transform: translateY(4%);
+  }
   :hover {
-    opacity: 0.8;
+    opacity: 0.9;
   }
 `;
