ecf3fc523863da24503034336522186f64ab0e64
anhmmo <aun@metropolia.fi>
2020-06-09 19:08:41 +0300

    style for thumbail

--- src/Components/styles/StyledMovieThumb.js
+++ src/Components/styles/StyledMovieThumb.js
@@ -1,12 +1,11 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledMovieThumb = styled.div`
   img {
     width: 100%;
-    /* min-height: 340px; */
+    height: 80%;
     transition: all 0.3s;
     object-fit: cover;
-    border-radius: 20px;
     animation: animateMovieThumb 0.5s;
 
     :hover {
@@ -25,5 +24,43 @@ export const StyledMovieThumb = styled.div`
         opacity: 1;
       }
     }
+  }
+
+  .content-box {
+    position: relative;
+    background-color: white;
+    padding: 10px;
+    margin-top: -10px;
+    height: 40px;
+    font-family: "Source Sans Pro", sans-serif;
+  }
+
+  .title {
+    position: absolute;
+    display: block;
+    color: #494c62;
+    font-size: 21px;
+    font-weight: 700;
+    top: 6px;
+  }
+
+  .genre {
+    position: absolute;
+    display: block;
+    bottom: 8px;
+    color: #babbc3;
+    font-size: 15px;
+  }
+
+  .box-rating {
+    position: absolute;
+    top: -28px;
+    right: 0;
+    color: #ff0079;
+    padding: 3px 8px;
+    border: 1.2px solid #ff0079;
+    border-radius: 3px;
+    background-color: white;
+    z-index: 2;
   }
 `;
