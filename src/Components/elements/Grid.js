b2ca338f1b7a4454cd3eed9f2d9626e6ad3dcdd5
anhmmo <aun@metropolia.fi>
2020-06-10 23:04:47 +0300

    style for popular section and filter menu

--- src/Components/elements/Grid.js
+++ src/Components/elements/Grid.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { StyledGrid, StyledGridContent } from '../styles/StyledGrid';
+import { StyledGrid, StyledGridContent } from "../styles/StyledGrid";
 
 const Grid = ({ header, children }) => (
   <StyledGrid>
-    <h1>{header}</h1>
+    <div className="film-option-box">
+      <div className="description-title">{header}</div>
+      <div className="description-icon">
+        <i class="fab fa-hotjar"></i>
+      </div>
+      <div className="filter-menu">
+        <div>name</div>
+        <div>year</div>
+        <div>genre</div>
+        <div>actor</div>
+        <div>cast</div>
+      </div>
+    </div>
+    {/* <h1>{header}</h1>*/}
     <StyledGridContent>{children}</StyledGridContent>
   </StyledGrid>
-)
+);
 
 Grid.propTypes = {
   header: PropTypes.string,
-}
+};
 
 export default Grid;
--- src/Components/styles/StyledGrid.js
+++ src/Components/styles/StyledGrid.js
@@ -12,6 +12,69 @@ export const StyledGrid = styled.div`
       font-size: 25px;
     }
   }
+
+  .film-option-box {
+    width: 50vw;
+    margin: 50px auto;
+    position: relative;
+    height: 115px;
+  }
+  .description-title {
+    padding: 14px;
+    width: 80%;
+    margin-left: 4%;
+    border-top-right-radius: 38px;
+    border-bottom-left-radius: 38px;
+    position: absolute;
+    font-size: 28px;
+    background-color: white;
+    box-shadow: 2px 5px 3px tomato;
+    -webkit-box-shadow: 2px 5px 3px tomato;
+    color: black;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    z-index: 3;
+    text-align: center;
+    text-shadow: 1px 1px 4px #000000;
+  }
+
+  .description-icon {
+    position: absolute;
+    width: 3.5%;
+    background-color: #ff415b;
+    box-shadow: 2px 5px 3px #d40e1a;
+    -webkit-box-shadow: 2px 5px 3px #d40e1a;
+    right: 25px;
+    padding: 18.5px;
+    border-bottom-right-radius: 30px;
+    border-top-left-radius: 30px;
+  }
+
+  .description-icon i {
+    text-align: center;
+    color: white;
+    font-size: 24px;
+  }
+  .filter-menu {
+    display: flex;
+    position: absolute;
+    bottom: -5px;
+    background-color: white;
+    width: 70%;
+    margin-left: 10%;
+    padding: 13px;
+    border-bottom-left-radius: 30px;
+  }
+  .filter-menu > div {
+    margin-left: 20px;
+    width: 20%;
+    background-color: #ff415b;
+    box-shadow: 2px 5px 3px #d40e1a;
+    -webkit-box-shadow: 2px 5px 3px #d40e1a;
+    text-align: center;
+    color: white;
+    padding: 8px;
+  }
 `;
 
 export const StyledGridContent = styled.div`
